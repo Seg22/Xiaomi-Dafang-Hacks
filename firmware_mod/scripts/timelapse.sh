e2dd7b03e3b18931cfe9a638bb32f5c26003254b
Firebirth <sergeneufeld@yahoo.de>
2019-07-30 09:57:40 +0200

    Add files via upload
    Added automatic Timelapse video rendering with new editable settings

--- firmware_mod/scripts/timelapse.sh
+++ firmware_mod/scripts/timelapse.sh
@@ -16,8 +16,8 @@ if [ -z "$TIMELAPSE_INTERVAL" ]; then TIMELAPSE_INTERVAL=2.0; fi
 
 # because``date`` doesn't support milliseconds +%N
 # we have to use a running counter to generate filenames
-counter=0
-last_prefix=''
+counter=1
+#last_prefix=''
 ts_started=$(date +%s)
 
 SAVE_DIR=$BASE_SAVE_DIR
@@ -28,6 +28,21 @@ SAVE_DIR=$BASE_SAVE_DIR
         mkdir -p $SAVE_DIR
     fi
 
+	if [ $TIMELAPSE_DURATION -eq 0 ]; then
+	TIMELAPSE_LENGTH=0
+	fi
+	
+	TIMELAPSE_INTERVAL_MIN=$TIMELAPSE_INTERVAL
+	
+	#determins the Timelapse intevale depending on the set timelapse length
+	if [ $TIMELAPSE_LENGTH != 0 ]; then
+        TIMELAPSE_INTERVAL=$(($TIMELAPSE_DURATION*60/$TIMELAPSE_LENGTH/30))
+    fi
+	
+	if [ $TIMELAPSE_INTERVAL -le $TIMELAPSE_INTERVAL_MIN ]; then
+	TIMELAPSE_INTERVAL=$TIMELAPSE_INTERVAL_MIN
+	fi
+	
 while true; do
     #filename_prefix="$(date +%Y-%m-%d_%H%M%S)"
     #if [ "$filename_prefix" = "$last_prefix" ]; then
@@ -43,6 +58,7 @@ while true; do
     else
         /system/sdcard/bin/getimage | /system/sdcard/bin/jpegoptim -m"$COMPRESSION_QUALITY" --stdin --stdout > "$SAVE_DIR/$filename" &
     fi
+		
     sleep $TIMELAPSE_INTERVAL
 
     if [ $TIMELAPSE_DURATION -gt 0 ]; then
@@ -58,21 +74,40 @@ done
 # loop completed so let's purge pid file
 rm "$PIDFILE"
 
-cd /system/sdcard/DCIM/timelapse/$(date +%Y-%m-%d)
+#switch current folder to timelapse
+cd $SAVE_DIR
 
-find . -type f -size -55000c -iname '*.jpg' > tmp1.txt
+wc -c *.jpg > tmp3.txt
+lines2=$(cat tmp3.txt | wc -l)
+sed -n $((lines2))p tmp3.txt > tmp4.txt
+B=$(grep -o "[0-9]*" tmp4.txt)
+average=$(($B/$lines2))
+
+#write all imagefiles over 55kb into temp file
+find . -type f -size -$(($average-$average/20))c -iname '*.jpg' > tmp1.txt
+#take only the numbers out of first temp file and write into another
 grep -o "[0-9]*" tmp1.txt > tmp2.txt
 
+#set counter and get number of lines 
 counter=1
-Zeilen=$(cat tmp2.txt | wc -l)
+lines=$(cat tmp2.txt | wc -l)
 
-while [ $counter -le $Zeilen ]; do
+while [ $counter -le $lines ]; do
+    #get the first line, then second line etc. 
 	X=$(sed -n $(($counter))p tmp2.txt)
-	cp img$(($X+1)).jpg img$X.jpg
+	#get first line, delete leading zeros add +1 and add leading zeros again
+	Y=$(echo $X | sed -e 's/^0*//')
+	Z=$(($Y+1))
+	A=$(printf '%05d' "$Z")
+	#Y+Z+A together
+	#H=$(printf '%05d' "$(($(($(echo $X | sed -e 's/^0*//')+1))))")
+	cp img$A.jpg img$X.jpg
 	counter=$(($counter + 1))
 	done
 rm tmp1.txt
 rm tmp2.txt
+rm tmp3.txt
+rm tmp4.txt
 
 /system/sdcard/bin/avconv -y -r 30 -f image2 -i img%05d.jpg  $(date +%Y-%m-%d_%H%M%S).mov
 #rm *.jpg
